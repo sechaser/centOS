35125932b7e8011cd4f2159d0f6eeffa840f0542
sechaser <413406236@qq.com>
2016-10-26 18:13:24 +0800

    Orphan process

--- main.c
+++ main.c
@@ -14,12 +14,14 @@ int main(void)
         exit(1);
     }
 
+    //Child process
     if(pid == 0)
     {
         printf("child process: pid = %d, ppid = %d\n", getpid(), getppid());
         sleep(5);
         printf("child process: pid = %d, ppid = %d\n", getpid(), getppid());
     }
+    //Father process
     else
         printf("father process: pid = %d\n", getpid());
 
